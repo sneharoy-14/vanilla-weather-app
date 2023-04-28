ea885ec41068349094e239f42aee100dbd157c43
sneharoy-14 <sneharoy3104@gmail.com>
2023-04-28 19:29:41 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector(`#temp-number`);
+  temperatureElement.innerHTML = `${temperature}`;
+  let description = document.querySelector(`#description`);
+  description.innerHTML = response.data.weather[0].main;
+  let humidity = document.querySelector(`#humidity`);
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.querySelector(`#wind-speed`);
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "c119ffef35b7245a5e03b6e5724ae961";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
