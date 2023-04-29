99374485703df73ff4040cafb72e0b01c21992f8
sneharoy-14 <sneharoy3104@gmail.com>
2023-04-29 11:03:36 +0100

    Updated the time and weather

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
-  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector(`#temp-number`);
   temperatureElement.innerHTML = `${temperature}`;
@@ -9,6 +31,8 @@ function displayTemp(response) {
   humidity.innerHTML = response.data.main.humidity;
   let windSpeed = document.querySelector(`#wind-speed`);
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let date = document.querySelector(`#date`);
+  date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c119ffef35b7245a5e03b6e5724ae961";
